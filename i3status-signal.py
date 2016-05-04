4835cdaf0ea3664cd64b06dc7aa97e35dc1fbb1b
Parantapa Bhattacharya <pb@parantapa.net>
2016-05-04 20:58:12 +0530

    Bugfix i3status-signal

--- i3status-signal.py
+++ i3status-signal.py
@@ -13,25 +13,35 @@ import os
 import signal
 from subprocess import Popen, PIPE
 
-from i3status import get_pids
-from pbapps_common import get_i3status_rundir
+import logbook
 
-def main():
+from i3status import get_pids
+from pbapps_common import get_i3status_rundir, get_logdir
+
+log = logbook.Logger("i3status-signal")
+
+def do_main():
+    """
+    Send the signal to the process.
+    """
+
     # Create the external state directory
     extdir = get_i3status_rundir()
 
     # Get the service pids
-    service_pids = sorted(get_pids(extdir))
+    service_pids = get_pids(extdir)
 
     # If we dont have any active services then exit
     if not service_pids:
+        log.info("No active services found.")
         return
 
     # Select the service name
     cmd = ["dmenu-wrap"]
     proc = Popen(cmd, stdin=PIPE, stdout=PIPE)
-    service, _ = proc.communicate("\n".join(service_pids))
+    service, _ = proc.communicate("\n".join(sorted(service_pids)))
     service = service.strip()
+    log.info("Selected service: {}", service)
 
     # Signal list
     signame_signum = {
@@ -47,15 +57,27 @@ def main():
     proc = Popen(cmd, stdin=PIPE, stdout=PIPE)
     signame, _ = proc.communicate("\n".join(signame_signum))
     signame = signame.strip()
+    log.info("Selected signam: {}", signal)
 
     # If we dont select any signal then exit.
     if signame == "-":
+        log.info("Got empty signal ..")
         return
 
     # Send signal to code
     pid = service_pids[service]
     signum = signame_signum[signame]
+
+    log.info("Sending signal {} to pid {} ...", signum, pid)
     os.kill(pid, signum)
+
+def main():
+    # Setup logfile
+    logfile = get_logdir() + "/i3status-signal.log"
+    logbook.FileHandler(logfile).push_application()
+
+    with log.catch_exceptions():
+        do_main()
 
 if __name__ == '__main__':
     main()
